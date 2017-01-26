8c81da922387b90f83d5dfa00693d392d0f16d0a
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2017-01-26 12:08:16 +0530

    Github pages init

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+//initialisation of the parallax in materialize
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+
+//Youtube Player initialisation script
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+        height: '300',
+        width: '500',
+        videoId: 'XZnF6HLu4oU'
+    });
+}
+
